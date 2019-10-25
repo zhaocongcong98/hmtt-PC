eb33b2c75cf3b8a41a528b500f52c03b46e562fb
zhaocongcong98 <1274908194@QQ.com>
2019-10-25 19:46:05 +0800

    登陆页面

---
+++ src/view/login/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="contation">login</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+   <el-card>
+      <img src="../../assets/logo_index.png" width="200px" alt />
+     <el-form ref="form" :model="form">
+  <el-form-item >
+    <el-input v-model="form.dainhu" placeholder="请输入手机号"></el-input>
+  </el-form-item>
+  <el-form-item >
+    <el-input v-model="form.mima" placeholder="请输入验证码" style="width:235px;margin-right:10px"></el-input>
+    <el-button>发送验证码</el-button>
+    </el-form-item>
+     <el-form-item >
+      <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+  </el-form-item>
+  <el-form-item>
+    <el-button type="primary" style="width:100%" >立即登陆</el-button>
+  </el-form-item>
+</el-form>
+   </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      form: {
+        dianhua: '',
+        mima: ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang='less'>
+.container{
+  width: 100%;
+  height: 100%;
+  background: url("../../assets/login_bg.jpg") no-repeat center / cover;
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+.el-card{
+  width: 400px;
+  height: 370px;
+  position: absolute;
+  left: 50%;
+  top:50%;
+  transform: translate(-50%,-50%);
+  // text-align: center;
+  img{
+    display: block;
+    margin: 0 auto 30px;
+  }
+}
+</style>
